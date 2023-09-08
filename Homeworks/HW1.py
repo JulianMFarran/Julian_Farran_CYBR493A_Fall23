0bb4939e65ff70a1ea4a79ae4cd0bf9c7013d965
JulianMFarran <142448928+JulianMFarran@users.noreply.github.com>
2023-09-08 16:59:30 -0400

    Created HW1 Python File.
    This console output will print First and Last Name, Class Name and Semester, And Prints Homework #1

---
+++ Homeworks/HW1.py
@@ -0,0 +1,15 @@
+# Homework 1
+# First Name: Julian
+# Last Name: Farran
+# Class: CYBR493A
+# Semester: Fall 2023
+
+# Print your first and last name
+print("Julian Farran")
+
+# Print class name and semester
+print("Class: CYBR493A")
+print("Semester: Fall 2023")
+
+# Print Homework #1
+print("Homework #1")
