bfc811f19e3c4a0ce745aa9ed65bc61ef9b5d445
JulianMFarran <142448928+JulianMFarran@users.noreply.github.com>
2023-09-11 16:14:24 -0400

    In class activity 2
    Pushed the code "An example of statements in pyhon: IF"

---
+++ In-class activities/IfStatementExample.py
@@ -0,0 +1,14 @@
+# Julian Farran
+# An example of statements in python
+# In class activity 2
+# 9/11/2023
+
+# value1
+value1 = input("Give me value1:")
+# value2
+value2 = input("Give me value2:")
+
+if value1 >= value2:
+    print(value1 + " is greater than or equal to " + value2)
+else:
+    print(value2 + " is greater than or equal to " + value1)
