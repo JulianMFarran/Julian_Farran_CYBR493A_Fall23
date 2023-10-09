308b021f98badb24392d4fd6a99d902f5fb14187
JulianMFarran <142448928+JulianMFarran@users.noreply.github.com>
2023-10-09 16:02:16 -0400

    Updated Cypher
    Could be improved but works as intended.

--- In-class activities/In_Class_Activity6_Ceaser.py
+++ In-class activities/In_Class_Activity6_Ceaser.py
@@ -5,10 +5,11 @@ alphabet = 'abcdefghijklmnopqrstuvwxyz'
 encryption_key = 4
 
 # Store a message to encrypt
-message = "Julian Farran"
+message = "radar"
 
 # Initialize an empty string for the encrypted message
 encrypted_message = ""
+
 
 # Function to encrypt a character using Caesar Cipher
 def encrypt_char(char, key):
@@ -20,10 +21,10 @@ def encrypt_char(char, key):
         return encrypted_char.upper() if is_upper else encrypted_char
     return char
 
+
 # Encrypt the message character by character
 for char in message:
     encrypted_message += encrypt_char(char, encryption_key)
 
 # Print the original message and the encrypted message
-print("Original Message:", message)
-print("Encrypted Message:", encrypted_message)
+print("Original Message:", message, "Encrypted Message:", encrypted_message)
