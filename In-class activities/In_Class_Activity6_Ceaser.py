718945620cf8c7292c324d465190455bb2e62169
JulianMFarran <142448928+JulianMFarran@users.noreply.github.com>
2023-10-09 15:48:32 -0400

    Updated Cesar Cypher to work correctly as anticipated
    Fixed in Class Activity 6.

--- In-class activities/In_Class_Activity6_Ceaser.py
+++ In-class activities/In_Class_Activity6_Ceaser.py
@@ -1,35 +1,29 @@
 # Store the alphabet in a variable named alphabet
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-# Store the encryption key 4 as of right now.
+# Store the encryption key as 4
 encryption_key = 4
 
-# Store a message to encrypt (You can put anything here even Zebra -> Difve, it works)
+# Store a message to encrypt
 message = "Julian Farran"
 
-# Initialize an empty string for the encrypted message.
+# Initialize an empty string for the encrypted message
 encrypted_message = ""
 
-# Create a dictionary to map characters to their indices
-#Instead of repeatedly searching for the index of a character in the alphabet string,
-# We should simply create a dictionary that maps each letter to its index.
-# This will improve the lookup efficiency.
-# (This will allow quick lookup of a character's index when needed in the encryption process.)
-char_to_index = {char: index for index, char in enumerate(alphabet)}
+# Function to encrypt a character using Caesar Cipher
+def encrypt_char(char, key):
+    if char.isalpha():
+        is_upper = char.isupper()
+        char = char.lower()
+        encrypted_index = (alphabet.index(char) + key) % 26
+        encrypted_char = alphabet[encrypted_index]
+        return encrypted_char.upper() if is_upper else encrypted_char
+    return char
 
-# Time to Encrypt the message, and handle the wrap around for encryption and handle uppercase and lowercase letters.
+# Encrypt the message character by character
 for char in message:
-    if char.isalpha():  # Check if the character is a letter or not
-        char_index = char_to_index[char.lower()]  # Find the position of the character in the alphabet
-        encrypted_index = (char_index + encryption_key) % 26  # Apply the Caesar Cipher logic with the wraparound
-        encrypted_char = alphabet[encrypted_index]  # Get the encrypted character
-        # Preserve the original case of the character so that it reads back correct.
-        if char.isupper():
-            encrypted_char = encrypted_char.upper()
-        encrypted_message += encrypted_char  # Append it to the result
-    else:
-        encrypted_message += char  # If it's not a letter, keep it unchanged
+    encrypted_message += encrypt_char(char, encryption_key)
 
-# Print the original message and then print the encrypted message below
+# Print the original message and the encrypted message
 print("Original Message:", message)
 print("Encrypted Message:", encrypted_message)
