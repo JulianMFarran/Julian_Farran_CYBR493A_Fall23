5204a1becea434cb0089157b0ce9ad16a6d60c96
JulianMFarran <142448928+JulianMFarran@users.noreply.github.com>
2023-11-06 15:45:06 -0500

    Finished in class 12
    fixed in class 12

--- In-class activities/In_Class_Activity12.py
+++ In-class activities/In_Class_Activity12.py
@@ -2,50 +2,64 @@ import requests
 from bs4 import BeautifulSoup
 
 class BugTrackingSystem:
-
     def Generate_Pages(self, initial_link):
-        # Create a list to store the links
-        page_links = [initial_link]
-
-        # Set a maximum number of pages you want to traverse (change as needed)
-        max_pages = 75
-
-        # Initialize a counter
-        page_counter = 0
-
         # Create a session to maintain cookies and headers for requests
         session = requests.Session()
 
-        while page_counter < max_pages and page_links:
-            # Get the next link from the list
-            link = page_links.pop(0)
+        # Set the maximum number of bugs to collect (412,622 in your case)
+        max_bugs = 412622
+        bugs_collected = 0
 
-            try:
-                # Send a GET request to the link
-                response = session.get(link)
+        # Calculate the number of pages to collect based on 75 bugs per page
+        max_pages = (max_bugs + 74) // 75  # Ceiling division
 
-                # Check if the request was successful
-                if response.status_code == 200:
-                    # Parse the HTML content of the page
-                    soup = BeautifulSoup(response.text, 'html.parser')
+        # Initialize a page counter
+        page_counter = 0
+        max_pages = 750
 
-                    # Print the current page link
-                    print(f"Page {page_counter + 1}: {link}")
+        while page_counter < max_pages:
+            # Calculate the page number and add it to the initial link
+            page_link = f"{initial_link}&start={page_counter * 75}"
+            page_counter += 1
+            print(page_link)
 
-                    # Extract links from the page and add them to the list
-                    for a_tag in soup.find_all('a', href=True):
-                        next_link = a_tag['href']
-                        if next_link.startswith("http"):
-                            page_links.append(next_link)
+            # try:
+            #     # Send a GET request to the page link
+            #     response = session.get(page_link)
+            #
+            #     # Check if the request was successful
+            #     if response.status_code == 200:
+            #         # Parse the HTML content of the page
+            #         soup = BeautifulSoup(response.text, 'html.parser')
+            #
+            #         # Extract bug data from the page and process it (modify this part as needed)
+            #         self.process_bug_data(soup)
+            #
+            #         # Update the bug counter
+            #         bugs_collected += min(75, max_bugs - bugs_collected)
+            #
+            #         # Print progress
+            #         print(f"Collected {bugs_collected}/{max_bugs} bugs")
+            #
+            #         # Increment the page counter
+            #         page_counter += 1
+            #
+            #     else:
+            #         print(f"Failed to retrieve page {page_counter + 1}. Status code: {response.status_code}")
+            #
+            # except Exception as e:
+            #     print(f"Error while processing page {page_counter + 1}: {e}")
 
-                    # Increment the page counter
-                    page_counter += 1
+    def process_bug_data(self, soup):
+        # Modify this function to extract and process bug data from the page
+        # You can extract bug information using BeautifulSoup and store it in your preferred format
 
-            except Exception as e:
-                print(f"Error while processing {link}: {e}")
+        # Example: extract and print bug titles
+        for bug_title in soup.find_all('span', class_='bug-title'):
+            print(bug_title.text.strip())
 
 # Usage example
 if __name__ == "__main__":
     bug_tracker = BugTrackingSystem()
-    initial_link = "https://bugs.launchpad.net/ubuntu/+bugs?field.searchtext=&field.status%3Alist=EXPIRED&field.status%3Alist=CONFIRMED&field.status%3Alist=TRIAGED&field.status%3Alist=INPROGRESS&field.status%3Alist=FIXCOMMITTED&field.status%3Alist=FIXRELEASED&field.importance%3Alist=UNKNOWN&field.importance%3Alist=UNDECIDED&field.importance%3Alist=CRITICAL&field.importance%3Alist=HIGH&field.importance%3Alist=MEDIUM&field.importance%3Alist=LOW&field.importance%3Alist=WISHLIST&field.information_type%3Alist=PUBLIC&field.information_type%3Alist=PUBLICSECURITY&field.information_type%3Alist=PRIVATESECURITY&field.information_type%3Alist=USERDATA&assignee_option=any&field.assignee=&field.bug_reporter=&field.bug_commenter=&field.subscriber=&field.structural_subscriber=&field.component-empty-marker=1&field.tag=&field.tags_combinator=ANY&field.status_upstream-empty-marker=1&field.has_cve.used=&field.omit_dupes.used=&field.omit_dupes=on&field.affects_me.used=&field.has_no_package.used=&field.has_patch.used=&field.has_branches.used=&field.has_branches=on&field.has_no_branches.used=&field.has_no_branches=on&field.has_blueprints.used=&field.has_blueprints=on&field.has_no_blueprints.used=&field.has_no_blueprints=on&search=Search&orderby=-importance&memo=75&start=0"
+    initial_link = "https://bugs.launchpad.net/ubuntu/+bugs?field.searchtext=&field.status%3Alist=EXPIRED&field.status%3Alist=CONFIRMED&field.status%3Alist=TRIAGED&field.status%3Alist=INPROGRESS&field.status%3Alist=FIXCOMMITTED&field.status%3Alist=FIXRELEASED&field.importance%3Alist=UNKNOWN&field.importance%3Alist=UNDECIDED&field.importance%3Alist=CRITICAL&field.importance%3Alist=HIGH&field.importance%3Alist=MEDIUM&field.importance%3Alist=LOW&field.importance%3Alist=WISHLIST&field.information_type%3Alist=PUBLIC&field.information_type%3Alist=PUBLICSECURITY&field.information_type%3Alist=PRIVATESECURITY&field.information_type%3Alist=USERDATA&assignee_option=any&field.assignee=&field.bug_reporter=&field.bug_commenter=&field.subscriber=&field.structural_subscriber=&field.component-empty-marker=1&field.tag=&field.tags_combinator=ANY&field.status_upstream-empty-marker=1&field.has_cve.used=&field.omit_dupes.used=&field.omit_dupes=on&field.affects_me.used=&field.has_no_package.used=&field.has_patch.used=&field.has_branches.used=&field.has_branches=on&field.has_no_branches.used=&field.has_no_branches=on&field.has_blueprints.used=&field.has_blueprints=on&field.has_no_blueprints.used=&field.has_no_blueprints=on&search=Search&orderby=-importance&memo=75"
     bug_tracker.Generate_Pages(initial_link)
