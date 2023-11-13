8b3c8630b4ae6af860bdc5abbe7bd2561959f2c7
JulianMFarran <142448928+JulianMFarran@users.noreply.github.com>
2023-11-13 16:15:37 -0500

    Fixed a bug with the link in class activity 13
    Fixed a bug where it would take you to page 1 with every link. I had to change the two starts at the end of the link so there was only one and it ensures you to get brought to the right page. Not page 1 every time.

--- In-class activities/In_Class_Activity_13.py
+++ In-class activities/In_Class_Activity_13.py
@@ -2,38 +2,38 @@ import requests
 from bs4 import BeautifulSoup
 
 def main():
-    main_link = "https://bugs.launchpad.net/ubuntu/+bugs?field.searchtext=&field.status%3Alist=EXPIRED&field.status%3Alist=CONFIRMED&field.status%3Alist=TRIAGED&field.status%3Alist=INPROGRESS&field.status%3Alist=FIXCOMMITTED&field.status%3Alist=FIXRELEASED&field.importance%3Alist=UNKNOWN&field.importance%3Alist=UNDECIDED&field.importance%3Alist=CRITICAL&field.importance%3Alist=HIGH&field.importance%3Alist=MEDIUM&field.importance%3Alist=LOW&field.importance%3Alist=WISHLIST&field.information_type%3Alist=PUBLIC&field.information_type%3Alist=PUBLICSECURITY&field.information_type%3Alist=PRIVATESECURITY&field.information_type%3Alist=USERDATA&assignee_option=any&field.assignee=&field.bug_reporter=&field.bug_commenter=&field.subscriber=&field.structural_subscriber=&field.component-empty-marker=1&field.tag=&field.tags_combinator=ANY&field.status_upstream-empty-marker=1&field.has_cve.used=&field.omit_dupes.used=&field.omit_dupes=on&field.affects_me.used=&field.has_no_package.used=&field.has_patch.used=&field.has_branches.used=&field.has_branches=on&field.has_no_branches.used=&field.has_no_branches=on&field.has_blueprints.used=&field.has_blueprints=on&field.has_no_blueprints.used=&field.has_no_blueprints=on&search=Search&orderby=-importance&memo=0&start=0"
+    main_link = "https://bugs.launchpad.net/ubuntu/+bugs?field.searchtext=&field.status%3Alist=EXPIRED&field.status%3Alist=CONFIRMED&field.status%3Alist=TRIAGED&field.status%3Alist=INPROGRESS&field.status%3Alist=FIXCOMMITTED&field.status%3Alist=FIXRELEASED&field.importance%3Alist=UNKNOWN&field.importance%3Alist=UNDECIDED&field.importance%3Alist=CRITICAL&field.importance%3Alist=HIGH&field.importance%3Alist=MEDIUM&field.importance%3Alist=LOW&field.importance%3Alist=WISHLIST&field.information_type%3Alist=PUBLIC&field.information_type%3Alist=PUBLICSECURITY&field.information_type%3Alist=PRIVATESECURITY&field.information_type%3Alist=USERDATA&assignee_option=any&field.assignee=&field.bug_reporter=&field.bug_commenter=&field.subscriber=&field.structural_subscriber=&field.component-empty-marker=1&field.tag=&field.tags_combinator=ANY&field.status_upstream-empty-marker=1&field.has_cve.used=&field.omit_dupes.used=&field.omit_dupes=on&field.affects_me.used=&field.has_no_package.used=&field.has_patch.used=&field.has_branches.used=&field.has_branches=on&field.has_no_branches.used=&field.has_no_branches=on&field.has_blueprints.used=&field.has_blueprints=on&field.has_no_blueprints.used=&field.has_no_blueprints=on&search=Search&orderby=-importance&memo=0"
 
     user_input = input('Enter "p" to get pages or "d" to display bug details: ')
 
     if user_input == 'p':
         page_number = int(input('Enter the page number to view (0-5508): '))
         if 0 <= page_number <= 5508:
-            page_links = generate_links(main_link)
-            selected_page_link = page_links[page_number]
+            page_links = generate_links(main_link, page_number)
+            selected_page_link = page_links[0]  # Use the first link
             print(f"Link for Page {page_number}: {selected_page_link}")
         else:
             print("Invalid page number. Please enter a number between 0 and 5508.")
     elif user_input == 'd':
         page_number = int(input('Enter the page number to display bugs: '))
         if 0 <= page_number <= 5508:
-            links = generate_links(main_link)
-            selected_page_link = links[page_number]
+            links = generate_links(main_link, page_number)
+            selected_page_link = links[0]  # Use the first link
             display_bugs_info(selected_page_link)
         else:
             print("Invalid page number. Please enter a number between 0 and 5508.")
     else:
         print("Invalid choice. Please enter 'p' or 'd.")
 
-def generate_links(start_link):
-    list_of_links = []
-
-    # Calculate the links to pages
-    for page_number in range(5509):  # 0 to 5508
-        page_link = f"{start_link}&start={page_number * 75}"
-        list_of_links.append(page_link)
-
-    return list_of_links
+def generate_links(start_link, page_number):
+    """
+        This method generates a link for a specific page
+        :param start_link: The start link of the bug tracking system.
+        :param page_number: The page number to view (0-5508)
+        :return: The link to the specific page
+    """
+    page_link = f"{start_link}&start={page_number * 75}"
+    return [page_link]  # Return as a list to match the previous structure
 
 def display_bugs_info(bugs_page):
     response = requests.get(bugs_page)
